617d3d03973695abe097d9680cec94919b5efede
malavikasrini21 <malavikasrini21@gmail.com>
2025-02-27 11:10:18 +0530

    wandb plot for sample image of each class in fashion_MNIST dataset

--- dataset.py
+++ dataset.py
@@ -2,21 +2,20 @@ import wandb
 import numpy as np
 from keras.datasets import fashion_mnist
 
-# Initialize wandb
+# Initializing wandb
 wandb.init(project="da6401_assignment1", name="fashion_mnist_dataset")
 
-# Load dataset
-(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+# Loading dataset
+(x_tr_imgs, y_tr_labels), (x_tst_imgs, y_tst_labels) = fashion_mnist.load_data()
 
-# Class names for labels
-class_names = ["T-shirt/top", "Trouser", "Pullover", "Dress", "Coat", 
-               "Sandal", "Shirt", "Sneaker", "Bag", "Ankle boot"]
+# fashion_mnist classes
+target_classes = ["T-shirt/top", "Trouser", "Pullover", "Dress", "Coat", "Sandal", "Shirt", "Sneaker", "Bag", "Ankle boot"]
 
-# Convert images to a format compatible with wandb (add color channel)
-train_images = np.expand_dims(train_images, axis=-1)  # Shape (28, 28) → (28, 28, 1)
+x_tr_imgs = np.expand_dims(x_tr_imgs, axis=-1)  # Shape (28, 28) → (28, 28, 1)
 
-# Log sample images to W&B
-wandb.log({"examples": [wandb.Image(train_images[i], caption=class_names[train_labels[i]]) for i in range(10)]})
+img=[]
+for i in range(len(target_classes)):
+    img.append(wandb.Image(x_tr_imgs[i], caption=target_classes[y_tr_labels[i]]))
 
-# Finish logging
+wandb.log({"Sample Images of Each class in Fashion_MNIST Dataset": img})
 wandb.finish()
