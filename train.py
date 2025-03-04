4dfe8d85ba0603420dfdf508aed3e2e4434d7b9d
malavikasrini21 <malavikasrini21@gmail.com>
2025-03-04 23:04:01 +0530

    multi_layer n/w with 70% accuracy

--- train.py
+++ train.py
@@ -24,8 +24,8 @@ def parse_args():
 
     # # Training Parameters
     parser.add_argument("-n", "--saz", type=int, default=10,help="sample images")
-    parser.add_argument("-e", "--epochs", type=int, default=5)
-    parser.add_argument("-b", "--batch_size", type=int, default=10)
+    parser.add_argument("-e", "--epochs", type=int, default=100)
+    parser.add_argument("-b", "--batch_size", type=int, default=48)
 
     # # Loss & Optimizer
     parser.add_argument("-l", "--loss", choices=["mean_squared_error", "cross_entropy"], default="cross_entropy")
@@ -41,9 +41,9 @@ def parse_args():
     parser.add_argument("-w_d", "--weight_decay", type=float, default=0.0)
 
     # # Model Parameters
-    parser.add_argument("-w_i", "--weight_init", choices=["random", "Xavier"], default="Xavier")
-    parser.add_argument("-nhl", "--num_layers", type=int, default=1)
-    parser.add_argument("-sz", "--hidden_size", type=int, default=512)
+    parser.add_argument("-w_i", "--weight_init", choices=["random", "Xavier"], default="random")
+    parser.add_argument("-nhl", "--num_layers", type=int, default=3)
+    parser.add_argument("-sz", "--hidden_size", type=int, default=128)
     parser.add_argument("-a", "--activation", choices=["identity", "sigmoid", "tanh", "ReLU"], default="sigmoid")
 
     return parser.parse_args()
@@ -59,23 +59,32 @@ def main():
     # Load Data
     X_train, y_train, X_test, y_test = load_data(args.dataset)
  
-    #test split
+    #small test split
     X_smp_test = X_test[:args.saz]
     y_smp_test = y_test[:args.saz]
 
+    splits=int(0.85 * X_train.shape[0])
     # train split
-    X_smp_train = X_train[(args.saz//3):args.saz]
-    y_smp_train = y_train[(args.saz//3):args.saz]
+    # X_smp_train = X_train[(args.saz//3):args.saz]
+    # y_smp_train = y_train[(args.saz//3):args.saz]
+    X_smp_train = X_train[:splits]
+    y_smp_train = y_train[:splits]
     # valid split 
-    X_smp_valid = X_smp_train[:args.saz//3]
-    y_smp_valid = y_smp_train[:args.saz//3]
+    # X_smp_valid = X_smp_train[:args.saz//3]
+    # y_smp_valid = y_smp_train[:args.saz//3]
+    X_smp_valid = X_train[splits:]
+    y_smp_valid = y_train[splits:]
 
-    print(X_smp_train.shape)
-    print(X_smp_valid.shape)
-    print(X_smp_test.shape)
-    print(y_smp_train.shape)
-    print(y_smp_valid.shape)
-    print(y_smp_test.shape)
+    # print(X_smp_train.shape)
+    # print(X_smp_valid.shape)
+    # print(X_smp_test.shape)
+    # print(y_smp_train.shape)
+    # print(y_smp_valid.shape)
+    # print(y_smp_test.shape)
+
+    print(f"Train set shape: {X_smp_train.shape}, {y_smp_train.shape}")
+    print(f"Validation set shape: {X_smp_valid.shape}, {y_smp_valid.shape}")
+    print(f"Test set shape: {X_smp_test.shape}, {y_smp_test.shape}")
     
     optimizer = get_optimizer(args.optimizer, None, args)
 
@@ -88,7 +97,8 @@ def main():
         activation=args.activation,
         weight_init=args.weight_init,
         loss_fn=args.loss,
-        optimizer=optimizer
+        optimizer=optimizer,
+        num_layers=args.num_layers
     )
     
     # # Get Optimizer
@@ -97,7 +107,7 @@ def main():
     # # Train Model
     # model.train(X_smp_train, y_smp_train, X_smp_test, y_smp_test, epochs=args.epochs, batch_size=args.batch_size, loss=args.loss, optimizer=optimizer)
     #model.train(X_smp_train, y_smp_train, X_smp_test, y_smp_test, epochs=args.epochs, batch_size=args.batch_size)
-    model.train(X_smp_train, y_smp_train,X_smp_train, y_smp_train, epochs=args.epochs, batch_size=args.batch_size)
+    model.train(X_smp_train, y_smp_train,X_smp_valid, y_smp_valid, epochs=args.epochs, batch_size=args.batch_size)
     # # Finish WandB
     # wandb.finish()
 
